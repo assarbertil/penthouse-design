1f8155b9a345b7d95f39f30e1a515c4130e0a8a5
Assar C <assar.classon@gmail.com>
2021-09-15 12:54:32 +0200

    changed export pattern

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,3 @@
 export { Text } from "./Text";
 export { Button } from "./Button";
 export { Input } from "./Input";
-
-export { IconCross, IconSearch } from "./Icons";
-
-export * from "./lib/animations";
-
-export { tailwindPreset } from "./tailwindPreset";

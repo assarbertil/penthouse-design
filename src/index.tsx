7b3d897dcd59cdc6058d2ba4bcac203e12701637
Assar C <assar.classon@gmail.com>
2021-09-15 12:33:31 +0200

    added 2 new animations

--- src/index.tsx
+++ src/index.tsx
@@ -6,4 +6,4 @@ export { IconCross, IconSearch } from "./Icons";
 
 export * from "./lib/animations";
 
-export { tailwindPreset, safelist } from "./tailwindPreset";
+export { tailwindPreset } from "./tailwindPreset";
--- src/lib/animations.ts
+++ src/lib/animations.ts
@@ -1,5 +1,5 @@
 /* 
-  Clickable items animations
+Clickable items animations
 */
 export const clickable = {
   hover: {
@@ -13,11 +13,33 @@ export const clickable = {
 };
 
 export const clickableWithScale = {
-  hover: {
-    ...clickable.hover,
+  hover: { ...clickable.hover },
+  tap: { ...clickable.tap, scale: 1.04 },
+};
+
+/* 
+Element appear animations
+*/
+export const appear = {
+  initial: { opacity: 0, y: 20 },
+  animate: { opacity: 1, y: 0 },
+};
+
+/* 
+  Staggering animations
+*/
+export const basicStaggering = {
+  initial: { opacity: 0 },
+  animate: {
+    opacity: 1,
+    transition: { staggerChildren: 0.05 },
   },
-  tap: {
-    ...clickable.tap,
-    scale: 1.04,
+};
+
+export const slowStaggering = {
+  initial: { opacity: 0 },
+  animate: {
+    opacity: 1,
+    transition: { staggerChildren: 1 },
   },
 };

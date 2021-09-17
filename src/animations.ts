900690f4f27ccce298454219be1bd7486ede9258
Assar C <assar.classon@gmail.com>
2021-09-17 14:21:42 +0200

    added exit animation to base appear

--- src/animations.ts
+++ src/animations.ts
@@ -18,11 +18,16 @@ export const clickableWithScale = {
 };
 
 /* 
-Element appear animations
+Element mount / unmount animations
 */
 export const appear = {
   initial: { opacity: 0, y: 20 },
   animate: { opacity: 1, y: 0 },
+  exit: {
+    opacity: 0,
+    y: -20,
+    transition: { duration: 0.4, type: "spring" },
+  },
 };
 
 /* 
